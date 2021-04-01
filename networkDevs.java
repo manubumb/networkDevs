cdee97ee7742a96decf0c1bddb4f6b3680b54fad
Manu Bumb <mbumb2k@yahoo.com>
2021-04-01 19:27:22 -0400

    Added code to gracefully handle a SocketException so it doesn't just stop

--- networkDevs.java
+++ networkDevs.java
@@ -3,6 +3,10 @@ import java.net.*;
 import java.util.Date;
 
 public class networkDevs {
+
+    /*
+        Helper function to write a message to the event log
+    */
     static void log(String msg) {
         try {
             FileWriter fw = new FileWriter("networkDevs.log", true);
@@ -17,27 +21,43 @@ public class networkDevs {
         int i = 0;
         String host = "192.168.0.";
         InetAddress temp;
+
+        // Check to make sure there's 1 arg (for now - in the future, I'll support a second arg for texting)
         if (args.length != 1) {
             System.out.println("Error: Must include final byte of starting address in command line");
         } else {
+            // got 1 arg!
             try {
                 // convert args[0] to an int for the for loop
                 i = Integer.parseInt(args[0]);
+                // make sure the supplied starting address makes sense
                 if ((i < 1) || (i > 255)) {
                     System.out.println("Error: args[0] must be a number between 1 and 255");
                 } else {
+                    // got a valid starting address, now loop through all addresses forever!
                     while (true) {
                         for (int j = i; j < 256; j++){
+                            // get an InetAddress so we can call isReachable() with it
                             temp = InetAddress.getByName(host+j);
-                            if (temp.isReachable(1000)){
-                                System.out.println(host+j + /*" (" + temp.getCanonicalHostName() +*/ " detected " + (new Date()));
-                                log((new Date()) + "\t" + host+j + " detected\r\n");
+                            try {
+                                if (temp.isReachable(1000)){
+                                    // got a device!  Display output and log it!
+                                    System.out.println((new Date()) +" "+ host+j + " detected");
+                                    log((new Date()) + "\t" + host+j + " detected\r\n");
 
-                                for (int k = 0; k < 5; k++) {
-                                    java.awt.Toolkit.getDefaultToolkit().beep();
-                                    Thread.sleep(800);
-                                }
-                            } //else System.out.println(host+i + " is not reachable");
+                                    // BEEP! BEEP! BEEP!
+                                    for (int k = 0; k < 3; k++) {
+                                        java.awt.Toolkit.getDefaultToolkit().beep();
+                                        Thread.sleep(800);
+                                    }
+                                }// else System.out.println(host+j + " is not reachable");
+                            } catch (SocketException e) {
+                                // One night, my internet stopped working and a SocketException was thrown and the program exited,
+                                // so catch the exception here and log it, then continue, that way the program doesn't just exit if
+                                // something happens to your network
+                                System.out.println((new Date()) + " Error: could not reach " + host+j + ". Check network connection");
+                                log ((new Date()) + " Error: could not reach " + host+j + ". Check network connection");
+                            }
                         } 
                     }
                 }
