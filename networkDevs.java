5f12d4f4b6c40e45308ea817535f06ac8a3d9860
Manu Bumb <mbumb2k@yahoo.com>
2021-04-07 18:18:55 -0400

    Added email! Now when a new device is detected, an email will be sent out to a recipient (pre-set in the code) using a gmail account (username/password pre-set in the code as well).

--- networkDevs.java
+++ networkDevs.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.*;
-import java.util.Date;
+import java.util.*;
+
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class networkDevs {
 
@@ -17,10 +20,23 @@ public class networkDevs {
         }
     }
 
-	public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException {
+	public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException, AddressException, MessagingException {
         int i = 0;
         String host = "192.168.0.";
         InetAddress temp;
+
+        String mailer = "smtpsend";
+        String mailhost = "smtp.gmail.com";
+        // REPLACE username and password
+        String username = "";
+        String password = "";
+        Properties props = System.getProperties();
+  
+        props.setProperty("mail.smtp.host", mailhost);
+        props.setProperty("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        // set any other needed mail.smtp.* properties here
+        Session session = Session.getInstance(props);
 
         // Check to make sure there's 1 arg (for now - in the future, I'll support a second arg for texting)
         if (args.length != 1) {
@@ -50,6 +66,23 @@ public class networkDevs {
                                         java.awt.Toolkit.getDefaultToolkit().beep();
                                         Thread.sleep(800);
                                     }
+
+                                    try{
+                                        MimeMessage msg = new MimeMessage(session);
+                                        // set the message content here
+                                        // msg.setFrom(new InternetAddress("@gmail.com"));
+                                        // REPLACE recipient
+                                        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("@gmail.com", false));
+                                        msg.setSubject("Device detected: " + host+j);
+                                        msg.setText((new Date()) +" "+host+j + " detected.");
+                                        msg.setHeader("X-Mailer", mailer);
+                                        msg.setSentDate(new Date());
+                            
+                                        Transport.send(msg, username, password);
+                                    }
+                                    catch (MessagingException m) {
+                                        m.printStackTrace();
+                                    }                            
                                 }// else System.out.println(host+j + " is not reachable");
                             } catch (SocketException e) {
                                 // One night, my internet stopped working and a SocketException was thrown and the program exited,
