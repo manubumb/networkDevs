e6a9576442fbeff69e682f20e0568b35547b30a6
Manu Bumb <mbumb2k@yahoo.com>
2021-04-08 17:38:27 -0400

    - updated the log function to include date and tab in the function itself - cleaned up the exceptions that are thrown by main - added support for entering a comma-separated list of recipient email addresses in the cmd line - started device search at 192.168.0.1 if the user doesn't enter a valid starting address - added exception handling for if the user doesn't enter a valid email address - added exception handling if the email couldn't be sent

--- networkDevs.java
+++ networkDevs.java
@@ -8,19 +8,20 @@ import javax.mail.internet.*;
 public class networkDevs {
 
     /*
-        Helper function to write a message to the event log
-    */
+     * Helper function to write a message to the event log
+     */
     static void log(String msg) {
         try {
             FileWriter fw = new FileWriter("networkDevs.log", true);
-            fw.write(msg);
+            fw.write((new Date()).toString());
+            fw.write("\t"+msg);
             fw.close();
         } catch (IOException e) {
             System.out.println("Error opening networkDevs.log");
         }
     }
 
-	public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException, AddressException, MessagingException {
+    public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException {
         int i = 0;
         String host = "192.168.0.";
         InetAddress temp;
@@ -31,73 +32,84 @@ public class networkDevs {
         String username = "";
         String password = "";
         Properties props = System.getProperties();
-  
+
         props.setProperty("mail.smtp.host", mailhost);
         props.setProperty("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.auth", "true");
-        // set any other needed mail.smtp.* properties here
+
         Session session = Session.getInstance(props);
 
-        // Check to make sure there's 1 arg (for now - in the future, I'll support a second arg for texting)
-        if (args.length != 1) {
-            System.out.println("Error: Must include final byte of starting address in command line");
-        } else {
-            // got 1 arg!
-            try {
-                // convert args[0] to an int for the for loop
-                i = Integer.parseInt(args[0]);
-                // make sure the supplied starting address makes sense
-                if ((i < 1) || (i > 255)) {
-                    System.out.println("Error: args[0] must be a number between 1 and 255");
-                } else {
-                    // got a valid starting address, now loop through all addresses forever!
-                    while (true) {
-                        for (int j = i; j < 256; j++){
-                            // get an InetAddress so we can call isReachable() with it
-                            temp = InetAddress.getByName(host+j);
-                            try {
-                                if (temp.isReachable(500)){
-                                    // got a device!  Display output and log it!
-                                    System.out.println((new Date()) +" "+ host+j + " detected");
-                                    log((new Date()) + "\t" + host+j + " detected\r\n");
+        // Check to make sure there's 2 args - in the future, I'll add support for prompting the user
+        if (args.length != 2) {
+            System.out.println("Error: Must include final byte of starting address and recipient\r\nemail address in command line");
+            return;
+        }
+        // got 2 args! first should be last byte of starting address, second should be
+        // an email address
 
-                                    // BEEP! BEEP! BEEP!
-                                    for (int k = 0; k < 3; k++) {
-                                        java.awt.Toolkit.getDefaultToolkit().beep();
-                                        Thread.sleep(800);
-                                    }
+        // convert args[0] to an int for the for loop
+        try {
+            i = Integer.parseInt(args[0]);
+        } catch (NumberFormatException nfe) {
+            i = 1;
+            System.out.println("args[0] must be an integer between 1 and 255. Starting search at " + host + i);
+            log("args[0] must be an integer between 1 and 255. Starting search at " + host+i + "\r\n");
+        }
 
-                                    try{
-                                        MimeMessage msg = new MimeMessage(session);
-                                        // set the message content here
-                                        // msg.setFrom(new InternetAddress("@gmail.com"));
-                                        // REPLACE recipient email or phone number
-//                                        msg.addRecipient(Message.RecipientType.TO, new InternetAddress("@gmail.com"));
-                                        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("@gmail.com", false));
-//                                        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("@txt.att.net", false));
-                                        msg.setSubject("Device detected: " + host+j);
-                                        msg.setText((new Date()) +" "+host+j + " detected.");
-                                        msg.setHeader("X-Mailer", mailer);
-                                        msg.setSentDate(new Date());
-                            
-                                        Transport.send(msg, username, password);
-                                    }
-                                    catch (MessagingException m) {
-                                        m.printStackTrace();
-                                    }                            
-                                }// else System.out.println(host+j + " is not reachable");
-                            } catch (SocketException e) {
-                                // One night, my internet stopped working and a SocketException was thrown and the program exited,
-                                // so catch the exception here and log it, then continue, that way the program doesn't just exit if
-                                // something happens to your network
-                                System.out.println((new Date()) + " Error: could not reach " + host+j + ". Check network connection");
-                                log ((new Date()) + " Error: could not reach " + host+j + ". Check network connection");
-                            }
-                        } 
-                    }
+        // make sure the supplied starting address makes sense
+        if ((i < 1) || (i > 255)) {
+            i = 1;
+            System.out.println("args[0] must be an integer between 1 and 255. Starting search at " + host + i);
+            log("args[0] must be an integer between 1 and 255. Starting search at " + host+i + "\r\n");
+        }
+
+        // got a valid starting address, now loop through all addresses forever!
+        while (true) {
+            for (int j = i; j < 256; j++) {
+                // get an InetAddress so we can call isReachable() with it
+                temp = InetAddress.getByName(host + j);
+                try {
+                    if (temp.isReachable(500)) {
+                        // got a device! Display output and log it!
+                        System.out.println((new Date()) + " " + host + j + " detected");
+                        log(host+j + " detected\r\n");
+
+                        // BEEP! BEEP! BEEP!
+                        for (int k = 0; k < 3; k++) {
+                            java.awt.Toolkit.getDefaultToolkit().beep();
+                            Thread.sleep(800);
+                        }
+
+                        // send the email
+                        try {
+                            MimeMessage msg = new MimeMessage(session);
+                            // set the message content here
+                            // msg.setFrom(new InternetAddress("@gmail.com"));
+                            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(args[1], true));
+                            // msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("@txt.att.net", false));
+                            msg.setSubject("Device detected: " + host + j);
+                            msg.setText((new Date()) + " " + host + j + " detected.");
+                            msg.setHeader("X-Mailer", mailer);
+                            msg.setSentDate(new Date());
+
+                            Transport.send(msg, username, password);
+                        } catch (AddressException ae) {
+                            System.out.println("Error: args[1] must be a series of comma-separated valid email addresses");
+                            log("Error: args[1] must be a series of comma-separated valid email addresses\r\n");
+                        } catch (SendFailedException se) {
+                            System.out.println("Message not sent: " + se.toString());
+                            log("Message not sent: " + se.toString());
+                        } catch (MessagingException me) {
+                            me.printStackTrace();
+                        }
+                    } // else System.out.println(host+j + " is not reachable");
+                } catch (SocketException se) {
+                    // One night, my internet stopped working and a SocketException was thrown and
+                    // the program exited, so catch the exception here and log it, then continue, that way the program
+                    // doesn't just exit if something happens to your network
+                    System.out.println((new Date()) + " Error: could not reach " + host+j + ". Check network connection");
+                    log("Error: could not reach " + host+j + ". Check network connection\r\n");
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Error: args[0] must be a number between 1 and 255");
             }
         }
     }
